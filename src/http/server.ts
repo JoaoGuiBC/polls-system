be5336f5626dc39053498f75bcebe1d0a12decbd
joaoguibc <joaoguibc@gmail.com>
2024-02-10 00:32:31 -0300

    Implement get-poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 
+import { getPoll } from './routes/get-poll'
 import { createPoll } from './routes/create-poll'
 
 const app = fastify()
 
+app.register(getPoll)
 app.register(createPoll)
 
 app.listen({ port: 3333 }).then(() => {
